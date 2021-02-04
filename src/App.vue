31277fb8cf3b24170bd8515a9dc346653c876ddb
dingxiaohuan <wydingxiaohuan@163.com>
2021-02-04 22:41:18 +0800

    侧边工具栏布局

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,39 @@
 <template>
   <div class="app">
     <h1>这个是页面内容</h1>
-    <visual-editor v-model="jsonData"/>
+    <VisualEditor v-model="jsonData" :config="visualConfig"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import { VisualEditor } from "@/packages/visual-editor";
+import { visualConfig } from "@/visual.config";
 
 export default defineComponent({
   name: "App",
   components: {
-    VisualEditor
+    VisualEditor,
   },
   data() {
     return {
+      visualConfig,
       jsonData: {
         container: {
           height: 500,
-          width: 800
+          width: 800,
         },
         blocks: [
           {
             top: 100,
-            left: 100
+            left: 100,
           },
           {
             top: 200,
-            left: 200
-          }
-        ]
-      }
+            left: 200,
+          },
+        ],
+      },
     };
   },
 });
--- src/packages/visual-editor.tsx
+++ src/packages/visual-editor.tsx
@@ -1,6 +1,6 @@
 import { computed, defineComponent, PropType } from "vue";
 import "./visual-editor.scss";
-import { VisualEditorModelValue } from "@/packages/visual-editor.utils";
+import { VisualEditorConfig, VisualEditorModelValue } from "@/packages/visual-editor.utils";
 import { useModel } from "./utils/useModel";
 import { VisualEditorBlock } from "./visual-editor-block";
 
@@ -10,6 +10,10 @@ export const VisualEditor = defineComponent({
       type: Object as PropType<VisualEditorModelValue>,
       required: true,
     },
+    config: {
+      type: Object as PropType<VisualEditorConfig>,
+      required: true
+    }
   },
   emits: {
     "update:modelValue": (val?: VisualEditorModelValue) => true,
@@ -19,16 +23,26 @@ export const VisualEditor = defineComponent({
       () => props.modelValue,
       (val) => ctx.emit("update:modelValue", val)
     );
-    console.log("dataModel", dataModel);
-
     const containerStyles = computed(() => ({
       width: `${dataModel.value.container.width}px`,
       height: `${dataModel.value.container.height}px`,
     }));
 
+    console.log(props, 'props');
+
     return () => (
       <div class="visual-editor">
-        <div class="visual-editor-menu">visual-editor-menu</div>
+        <div class="visual-editor-menu">
+          {props.config.componentList.map(component => (
+            <div class="visual-editor-menu-item">
+              <span class="visual-editor-menu-item-label">{component.label}</span>
+              <div class="visual-editor-menu-item-content">
+                
+              </div>
+              {component.preview()}
+            </div>
+          ))}
+        </div>
         <div class="visual-editor-head">visual-editor-head</div>
         <div class="visual-editor-operator">visual-editor-operator</div>
         <div class="visual-editor-body">
