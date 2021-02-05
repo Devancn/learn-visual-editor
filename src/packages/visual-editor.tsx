38b4684883ebdd821aeff526e5ddffd0f28fbe14
dingxiaohuan <wydingxiaohuan@163.com>
2021-02-05 23:16:13 +0800

    工具拖拽

--- src/packages/visual-editor.tsx
+++ src/packages/visual-editor.tsx
@@ -1,6 +1,10 @@
-import { computed, defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType, ref } from "vue";
 import "./visual-editor.scss";
-import { VisualEditorConfig, VisualEditorModelValue } from "@/packages/visual-editor.utils";
+import {
+  VisualEditorComponent,
+  VisualEditorConfig,
+  VisualEditorModelValue,
+} from "@/packages/visual-editor.utils";
 import { useModel } from "./utils/useModel";
 import { VisualEditorBlock } from "./visual-editor-block";
 
@@ -12,8 +16,8 @@ export const VisualEditor = defineComponent({
     },
     config: {
       type: Object as PropType<VisualEditorConfig>,
-      required: true
-    }
+      required: true,
+    },
   },
   emits: {
     "update:modelValue": (val?: VisualEditorModelValue) => true,
@@ -23,22 +27,81 @@ export const VisualEditor = defineComponent({
       () => props.modelValue,
       (val) => ctx.emit("update:modelValue", val)
     );
+
+    const containerRef = ref({} as HTMLDivElement);
     const containerStyles = computed(() => ({
       width: `${dataModel.value.container.width}px`,
       height: `${dataModel.value.container.height}px`,
     }));
 
-    console.log(props, 'props');
+    const menuDraggier = {
+      current: {
+        component: null as null | VisualEditorComponent,
+      },
+      dragstart: (e: DragEvent, component: VisualEditorComponent) => {
+        containerRef.value.addEventListener(
+          "dragenter",
+          menuDraggier.dragenter
+        );
+        containerRef.value.addEventListener("dragover", menuDraggier.dragover);
+        containerRef.value.addEventListener(
+          "dragleave",
+          menuDraggier.dragleave
+        );
+        containerRef.value.addEventListener("drop", menuDraggier.drop);
+        menuDraggier.current.component = component;
+      },
+      dragenter: (e: DragEvent) => {
+        e.dataTransfer!.dropEffect;
+      },
+      dragover: (e: DragEvent) => {
+        e.preventDefault();
+      },
+      dragleave: (e: DragEvent) => {
+        e.dataTransfer!.dropEffect = "none";
+      },
+      dragend: () => {
+        containerRef.value.removeEventListener(
+          "dragenter",
+          menuDraggier.dragenter
+        );
+        containerRef.value.removeEventListener(
+          "dragover",
+          menuDraggier.dragover
+        );
+        containerRef.value.removeEventListener(
+          "dragleave",
+          menuDraggier.dragleave
+        );
+        containerRef.value.removeEventListener("drop", menuDraggier.drop);
+        menuDraggier.current.component = null;
+      },
+      drop: (e: DragEvent) => {
+        const blocks = dataModel.value.blocks || [];
+        blocks.push({
+          top: e.offsetY,
+          left: e.offsetX,
+        });
+        dataModel.value = {
+          ...dataModel.value,
+          blocks
+        };
+      },
+    };
 
     return () => (
       <div class="visual-editor">
         <div class="visual-editor-menu">
-          {props.config.componentList.map(component => (
-            <div class="visual-editor-menu-item">
-              <span class="visual-editor-menu-item-label">{component.label}</span>
-              <div class="visual-editor-menu-item-content">
-                
-              </div>
+          {props.config.componentList.map((component) => (
+            <div
+              class="visual-editor-menu-item"
+              draggable
+              onDragend={menuDraggier.dragend}
+              onDragstart={(e) => menuDraggier.dragstart(e, component)}
+            >
+              <span class="visual-editor-menu-item-label">
+                {component.label}
+              </span>
               {component.preview()}
             </div>
           ))}
@@ -47,7 +110,11 @@ export const VisualEditor = defineComponent({
         <div class="visual-editor-operator">visual-editor-operator</div>
         <div class="visual-editor-body">
           <div class="visual-editor-content">
-            <div class="visual-editor-container" style={containerStyles.value}>
+            <div
+              class="visual-editor-container"
+              style={containerStyles.value}
+              ref={containerRef}
+            >
               {(dataModel.value?.blocks || []).map((block, index) => (
                 <VisualEditorBlock block={block} key={index} />
               ))}
